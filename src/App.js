bf3b6c1c46a2ba0b4f16e6d156a81b15ad8f2296
tan <buitan561993@gmail.com>
2023-02-15 13:32:52 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect ,useLayoutEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setCount] = useState(0)
+  useLayoutEffect(() => {
+    if (count > 3) {
+      setCount(0)}},[count])
+return (
+  <div className="App">
+    <h1>{count}</h1>
+    <button onClick={() => setCount(count + 1)}>Count</button>
+  </div>
+);
 }
 
 export default App;
